f10667bb4f4d999509fb68ed5ac418c19cf80afd
James Collerton <James.Collerton@kobaltmusic.com>
2018-09-27 09:59:45 +0100

    Added front end project

---
+++ src/main/java/com/artservicefrontend/artservicefrontend/ArtservicefrontendApplication.java
@@ -0,0 +1,12 @@
+package com.artservicefrontend.artservicefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtservicefrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtservicefrontendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/artservicefrontend/artservicefrontend/ArtservicefrontendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.artservicefrontend.artservicefrontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArtservicefrontendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
