0b75543fd83f5093de29d4964560c5faf4ad4f1f
James Collerton <James.Collerton@kobaltmusic.com>
2018-09-28 15:33:40 +0100

    Front end now registered to Eureka as well

---
+++ src/main/java/com/artservicefrontend/artservicefrontend/ArtServiceFrontEndApplication.java
@@ -0,0 +1,11 @@
+package com.artservicefrontend.artservicefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtServiceFrontEndApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArtServiceFrontEndApplication.class, args);
+	}
+}
