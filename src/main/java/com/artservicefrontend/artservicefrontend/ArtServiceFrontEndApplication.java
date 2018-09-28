e1e4ed6ea7067b3c296c46f405ddf68f606d4428
James Collerton <James.Collerton@kobaltmusic.com>
2018-09-28 16:20:30 +0100

    Added TODO

--- src/main/java/com/artservicefrontend/artservicefrontend/ArtServiceFrontEndApplication.java
+++ src/main/java/com/artservicefrontend/artservicefrontend/ArtServiceFrontEndApplication.java
@@ -3,6 +3,12 @@ package com.artservicefrontend.artservicefrontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+TODO: Comments
+TODO: Swagger
+TODO: Testing
+TODO: Docker
+ */
 @SpringBootApplication
 public class ArtServiceFrontEndApplication {
 	public static void main(String[] args) {
